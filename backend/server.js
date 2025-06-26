0289d82abf3a73e30233d235544f3b7338b618e4
G2Me-B <brutehackenstein@gmail.com>
2025-06-26 20:59:26 +0000

    updated the server.js

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,29 @@ import path from "path"
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 
 const __dirname = path.resolve()
 
 app.use(express.json()); // Middleware to parse json data from client (req.body)
-app.use("/api/products",productRoutes)
+app.use("/api/products", productRoutes)
 
-if(process.env.NODE_ENV ==="production"){
-  app.use(express.static(path.join(__dirname,'/frontend/dist'))) 
-  
-  app.get("",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get("", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   })
 }
 
 // console.log(process.env.MONGO_URI) 
-// Port activation for listening
-app.listen(PORT, () => {
-    connectDB()
+
+connectDB().then(() => {
+  // Port activation for listening
+  app.listen(PORT, () => {
+
     console.log(`Server started at http://localhost:${PORT}`)
+  })
+
 })
 
